5e245503e7e37569b126c6a3565e346ec7dab6e9
TranPhuocDat1 <100539942+TranPhuocDat1@users.noreply.github.com>
2022-07-12 09:44:07 +0700

    12
    214

--- resources/views/admin/room/create.blade.php
+++ resources/views/admin/room/create.blade.php
@@ -1,4 +1,4 @@
-@extends('home')
+@extends('admin.dashboard')
 @section('title', 'Add Room')
 @section('main')
 <form action="{{route('room.store')}}" method="POST" role="form" enctype="multipart/form-data">
