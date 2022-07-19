20abe566d28c67f7363d9427310cff460436f940
hocnb131 <83727424+hocnb131@users.noreply.github.com>
2022-07-19 23:03:55 +0700

    summernote

--- resources/views/admin/branch/create.blade.php
+++ resources/views/admin/branch/create.blade.php
@@ -36,7 +36,7 @@
             <div class="form-group">
                 <label for="">Description</label>
                 
-                <textarea name="description" class="form-control" id="content" placeholder="Input description"></textarea>
+                <textarea name="description" class="form-control" id="summernote" placeholder="Input description"></textarea>
                 
                 @error('description')
                 <small class="badge badge-danger">{{$message}}</small>
@@ -112,16 +112,32 @@
     </div>
     <button type="submit" class="btn btn-primary">Save Data</button>
 </form>
-@stop();
-<!-- @section('css')
-<link rel="stylesheet" href="{{url('/css')}}/plugins/summernote/summernote-bs4.min.css">
-@stop();
-@section('js')
+
 <script src="{{url('/css')}}/plugins/summernote/summernote-bs4.min.js"></script>
 <script>
-    $(function(){
-        $('#content').summernote();
-    });
-    // alert(212);
+    // $(function(){
+    //     $('#content').summernote();
+    // });
+    $('#summernote').summernote({
+        placeholder: 'Hello Bootstrap 4',
+        tabsize: 2,
+        height: 150,
+        focus: true ,
+      });
 </script>
-@stop(); -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+@stop();
+@section('css')
+<link rel="stylesheet" href="{{url('/css')}}/plugins/summernote/summernote-bs4.min.css">
+@stop();
+
+
+
+
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,29 +1,8 @@
 <?php
 $menu = config('menu');
-// dd($menu);
 ?>
 @extends('layouts.app')
-
 @section('content')
-<!-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                        
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,10 +21,20 @@ $menu = config('menu');
   <!-- Theme style -->
   <link rel="stylesheet" href="{{url('/css')}}/dist/css/adminlte.min.css">
   @yield('js')
-
+  <script src="{{url('/css')}}/plugins/summernote/summernote-bs4.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   @yield('css')
+  <link rel="stylesheet" href="{{url('/css')}}/plugins/summernote/summernote-bs4.min.css">
+  <!--  -->
+  <link rel="stylesheet" href="{{url('/css')}}/plugins/summernote/summernote-bs4.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
